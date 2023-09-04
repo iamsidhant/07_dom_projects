ee7e58838fa7bad127fb5405797141e5186adcd1
Sidhant Arya <121373417+iamsidhant@users.noreply.github.com>
2023-09-04 11:16:34 +0000

    Upto checkGuess

--- 4-GuessTheNumber/guess.js
+++ 4-GuessTheNumber/guess.js
@@ -14,13 +14,43 @@ let numGuess = 1;
 
 let playGame = true;
 
+if (playGame) {
+    submit.addEventListener('click', function(e){
+        e.preventDefault();
+        const guess = parseInt(userInput.value)
+        console.log(guess);
+        validateGuess()
+    })
+}
 
 function validateGuess(guess) {
-    //
+    if (isNaN(guess)) {
+        alert("Please enter a valid number")
+    } else if (guess < 1) {
+        alert("Please enter a number greater than 1")
+    } else if (guess > 100) {
+        alert("Please enter a number less than 100")
+    } else {
+        prevGuess.push(guess);
+        if (prevGuess === 11) {
+            prevGuess(guess);
+            displayMessage(`Game Over. Random number was ${randomNumber}`);
+            endGame();
+        } else {
+            displayGuess(guess);
+            checkGuess(guess);
+        }
+    }
 }
 
 function checkGuess(guess) {
-    //
+    if (guess === randomNumber) {
+        displayMessage(`Your guess is right`)
+    } else if (guess < randomNumber) {
+        displayMessage(`Number is too low`)
+    } else if (guess > randomNumber) {
+        displayMessage(`Number is too HIGH`)
+    }
 }
 
 function displayGuess(guess) {
